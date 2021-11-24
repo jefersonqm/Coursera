f45bb5c721415397f4d246e90148c7bd32fb37c2
Jeferson Mello <94336664+jefersonqm@users.noreply.github.com>
2021-11-24 01:09:30 -0300

    Update

--- Week_one/matrices.py
+++ Week_one/matrices.py
@@ -2,7 +2,6 @@ def create_matrix(num_rows, num_columns):
     '''(int, int) -> matrix, list of lists'''
     #empty list
     matrix = []
-
     #create row
     for i in range(num_rows):
         rows = []
--- Week_one/tasks.py
+++ Week_one/tasks.py
@@ -2,5 +2,7 @@ def main():
     a = [0,1,2,3,4]
     b = a[:]
     b[1] = 7
+    print("A =", a)
+    print("B =", b)
 
 main()
