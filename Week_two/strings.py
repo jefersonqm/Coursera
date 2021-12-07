f3a8486ae66981e408823a921f99f6f9d973ecd2
Jeferson Mello <94336664+jefersonqm@users.noreply.github.com>
2021-12-07 00:22:58 -0300

    concluded

--- Week_two/strings.py
+++ Week_two/strings.py
@@ -1,13 +1,13 @@
 def mais_curto(lista):
-    #função recebe uma lista de strings e retorna a mais curta
-    #ignora espaços
-    #retorna a string capitalizada
-    
+    #Função recebe uma lista de strings.
+    #Retorna a palavra com a menor quantidade de caracteres.
+    #Ignora espaços.
+    #Retorna a string capitalizada
+
     remove_espaços = []
     for palavras in lista:
         remove_espaços.append(palavras.strip())
+        ordena_tamanho = []
+        ordena_tamanho.extend(sorted(remove_espaços, key=len))
 
-    ordena_tamanho = []
-    ordena_tamanho.append(sorted(remove_espaços, key=len))
-
-    return ordena_tamanho[0]
+    return (ordena_tamanho[0].capitalize())
