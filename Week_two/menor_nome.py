4c54346eb98b1ef2d66e23ac0c7e4c5715f31759
Jeferson Mello <94336664+jefersonqm@users.noreply.github.com>
2021-12-10 16:10:25 -0300

    Erro corrigido: print no lugar do return.

--- Week_two/menor_nome.py
+++ Week_two/menor_nome.py
@@ -9,7 +9,7 @@ def menor_nome(lista):
     lista3 = sorted(lista2)
     
     if len(lista3[0]) == len(lista3[1]):
-        print(lista2[0])
+        return lista2[0]
     else:
         while len(lista3[0]) > len(lista3[1]):
             lista3 = lista3[1:]
