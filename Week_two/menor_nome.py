2e4ea0bb02689a629d7ec33484690c26acf6365d
Jeferson Mello <94336664+jefersonqm@users.noreply.github.com>
2021-12-10 16:05:49 -0300

    concluded

--- Week_two/menor_nome.py
+++ Week_two/menor_nome.py
@@ -7,9 +7,10 @@ def menor_nome(lista):
 
     lista3 = []
     lista3 = sorted(lista2)
-    print("Lista 3 =",lista3)
-
-    while len(lista3[0]) > len(lista3[1]):
-        lista3 = lista3[1:]
     
-    return lista3[0]
+    if len(lista3[0]) == len(lista3[1]):
+        print(lista2[0])
+    else:
+        while len(lista3[0]) > len(lista3[1]):
+            lista3 = lista3[1:]
+        return lista3[0]
