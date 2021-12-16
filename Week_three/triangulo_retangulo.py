1484dcdfeb5791883c3127126ccc17b9fd1c8ea3
Jeferson Mello <94336664+jefersonqm@users.noreply.github.com>
2021-12-16 00:49:41 -0300

    update

---
+++ Week_three/triangulo_retangulo.py
@@ -0,0 +1,29 @@
+class Triangulo:
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+    
+    def perimetro(self):
+        return self.a + self.b + self.c
+
+    def tipo_lado(self):
+        if self.a != self.b and self.b != self.c and self.a != self.c: 
+            return "escaleno"
+        elif self.a == self.b and self.b == self.c:
+            return "equilátero"
+        else:
+            return "isósceles"
+    
+    def retangulo(self):
+        quadrado_catetos = self.a**2 + self.b**2 
+        quadrado_hipotenusa = self.c**2
+
+        return quadrado_catetos == quadrado_hipotenusa
+
+    def semelhante(self, triangulo):
+        
+        return
+
+
