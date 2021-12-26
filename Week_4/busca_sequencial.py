c9bac1dcbffef6b352810a0302d51995730e90f0
jefersonqm-git <jeferson_qdm@hotmail.com>
2021-12-25 21:04:19 -0300

    update

--- Week_4/busca_sequencial.py
+++ Week_4/busca_sequencial.py
@@ -1,11 +1,11 @@
 def busca_sequencial(seq, x):
-
+      
     for i in range(len(seq)):
         if seq[i] == x:
             return True
     return False
 
-
 if __name__ == "__main__":
-    seq = [1,2,4,5,6,7,8,9,10]
-    x = 7
+
+    seq = [0,1,2,4,5,6,7,8,9,10]
+    print(busca_sequencial(seq, 7))    
