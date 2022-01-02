7b95d1a8504b5fe0d363c64f753bab901d222dcd
jefersonqm-git <jeferson_qdm@hotmail.com>
2022-01-01 23:00:44 -0300

    Upload in Archlinux

---
+++ Week_4/incomodam.py
@@ -0,0 +1,5 @@
+def incomdomam(n):
+    if not int > 0:
+        return " " * n
+    else:
+        "incomodam" * n
---
+++ Week_4/soma_lista.py
@@ -0,0 +1,5 @@
+def soma_lista(lista):
+    if not int > 0:
+        return " " * n
+    else:
+        "incomodam" * n
